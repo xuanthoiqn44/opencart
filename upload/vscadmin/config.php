c9aac8d61ddf8adedcab3ee84aa78b44ac7ccc96
root <root@ip-172-31-31-240.us-east-2.compute.internal>
2020-08-24 14:19:53 +0000

    change admin folder

---
+++ upload/vscadmin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://www.marbletilesale.com/vscadmin/');
+define('HTTP_CATALOG', 'https://www.marbletilesale.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.marbletilesale.com/vscadmin/');
+define('HTTPS_CATALOG', 'https://www.marbletilesale.com/');
+
+// DIR
+define('DIR_APPLICATION', '../vscadmin/');
+define('DIR_SYSTEM', '../system/');
+define('DIR_IMAGE', '../image/');
+define('DIR_STORAGE', '../../storage/');
+define('DIR_CATALOG', '../catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '8Hpc297whyhesqo9vSR4b3CXrSgN');
+define('DB_DATABASE', 'marbletilesale_com');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
