7447916b0fe82f544cf3b2742c33d04eb5087d32
Xuan Thoi <xuanthoiqn44@gmail.com>
2020-08-25 22:48:58 +0700

    update source

--- upload/admin/language/en-gb/extension/theme/journal3.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Heading
-$_['heading_title'] = 'Journal Theme' . (defined('JOURNAL3_ENV') && JOURNAL3_ENV === 'development' ? ' Opencart 3' : '');
--- upload/admin/language/en-gb/journal3/journal3.php
+++
@@ -1,2 +0,0 @@
-<?php
-$_['error_input_not_found'] = '%s: Parameter %s is not found!';
